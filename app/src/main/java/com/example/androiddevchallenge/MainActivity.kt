a6f9dda21ccdb581dbcf76dfc6a266473d7c14ba
poppito <underseer@gmail.com>
2021-03-07 14:36:36 +1100

    add some countdown timer logic, animate visibility, add font

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,8 +16,12 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,70 +32,183 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.util.isNumber
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    MyApp()
+                    AppState()
                 }
             }
         }
     }
 }
 
+private lateinit var timer: CountDownTimer
+
 // Start building your app here!
+@ExperimentalAnimationApi
 @Composable
-fun MyApp() {
+fun MyApp(text: MutableState<String>) {
     Column(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val text = remember(calculation = { mutableStateOf("") })
-        Text(
-            text = stringResource(id = R.string.txt_set_timer),
-            style = MaterialTheme.typography.h3
-        )
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth(0.8f),
-                value = text.value,
-                onValueChange = { text.value = it }
+        val error = rememberSaveable { mutableStateOf(false) }
+        val timerText = rememberSaveable { mutableStateOf("") }
+        val timerSet = rememberSaveable { mutableStateOf(false) }
+
+        AnimatedVisibility(visible = !timerSet.value) {
+            Text(
+                text = stringResource(id = R.string.txt_set_timer),
+                style = MaterialTheme.typography.h3
             )
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Go")
+        }
+        AnimatedVisibility(visible = !timerSet.value) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .padding(16.dp),
+                    value = text.value,
+                    onValueChange = {
+                        text.value = it
+                        error.value = (!it.isNumber() && !it.equals(""))
+                    },
+                    label = { Text("Enter number in seconds") },
+                    isError = error.value
+                )
+                Text(
+                    text = if (text.value.isNotEmpty()) stringResource(id = R.string.txt_seconds)
+                    else stringResource(
+                        id = R.string.txt_second
+                    ),
+                )
             }
         }
-        Text(text = "", modifier = Modifier.fillMaxWidth())
+        AnimatedVisibility(visible = !timerSet.value) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = {
+                        val timerIsSet = timerSet.value
+                        if (timerIsSet) {
+                            timer.cancel()
+                            timerText.value = ""
+                            timerSet.value = false
+                        } else {
+                            startTimer(
+                                timerText = timerText,
+                                count = text.value.toLong() * 1000
+                            )
+                            timer.start()
+                            timerSet.value = true
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    enabled = !error.value
+                ) {
+                    val timerIsSet = timerSet.value
+                    Text(
+                        text = stringResource(id = R.string.btn_go)
+                    )
+                }
+            }
+        }
+        AnimatedVisibility(visible = timerSet.value) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = timerText.value,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.h3
+                )
+            }
+        }
+
+        AnimatedVisibility(visible = timerSet.value) {
+            Button(
+                onClick = {
+                    timer.cancel()
+                    timerSet.value = false
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.btn_cancel)
+                )
+            }
+        }
     }
 }
 
+private fun startTimer(timerText: MutableState<String>, count: Long) {
+    timer = object : CountDownTimer(count, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            timerText.value = (millisUntilFinished / 1000L).toString()
+        }
+
+        override fun onFinish() {
+            timerText.value = ""
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun AppState() {
+    val text = rememberSaveable { mutableStateOf("") }
+    MyApp(text = text)
+}
+
+@ExperimentalAnimationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        MyApp()
+        AppState()
     }
 }
 
+@ExperimentalAnimationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
-        MyApp()
+        AppState()
     }
 }
